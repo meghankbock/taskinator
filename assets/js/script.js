ec2e7feb0ac0665a4bb00b9b44ef1433ce9ab729
Meghan Bock <meghan.bock@gmail.com>
2021-05-27 00:48:29 -0500

    final final changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,12 +73,15 @@ var createTaskEl = function (taskDataObj) {
 
   // add entire list item to list
   if(taskDataObj.status === "to do") {
+    listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
     tasksToDoEl.appendChild(listItemEl);
   }
   else if (taskDataObj.status === "in progress") {
+    listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
     tasksInProgressEl.appendChild(listItemEl);
   }
   else if (taskDataObj.status === "complete") {
+    listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
     tasksCompletedEl.appendChild(listItemEl);
   }
 
