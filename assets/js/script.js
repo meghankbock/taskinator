acafb8607603e4553bc4450e8dc91ab856701e56
Meghan Bock <meghan.bock@gmail.com>
2021-05-26 19:46:48 -0500

    add task input form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
-//console.log("save-task " + buttonEl);
+var formEl = document.querySelector("#task-form");
+//console.log("save-task " + formEl);
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    //console.log(event);
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
