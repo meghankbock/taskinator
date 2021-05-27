b57312b32a6dcc8e30c5fdd9b33c0225f1543f29
Meghan Bock <meghan.bock@gmail.com>
2021-05-26 20:06:39 -0500

    append form input to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,30 @@ var createTaskHandler = function(event) {
     event.preventDefault();
     //console.log(event);
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //console.dir(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // giv it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>"+ taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    //listItemEl.textContent = taskNameInput;
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+    //console.dir(listItemEl);
+
+
 }
 
 formEl.addEventListener("submit", createTaskHandler);
